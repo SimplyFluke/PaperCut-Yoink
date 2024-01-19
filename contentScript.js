079d971cdafd1f6692d4cc98db7203cef632e83e
Dadle Duppsko <76550302+SimplyFluke@users.noreply.github.com>
2024-01-19 09:39:56 +0100

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -4,28 +4,13 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "readAllText") {
     console.log("Content Script: Received readAllText message");
 
-    // Function to wait for the input field to be populated
-    function waitForInputField() {
-      console.log("Content Script: Waiting for Input Field");
+    // Use the provided JS path to locate the input field
+    const inputField = document.querySelector("#content > div > div > div.tabContent > div.box > table > tbody > tr:nth-child(2) > td.box-content > form > table > tbody > tr:nth-child(1) > td > p:nth-child(5) > input[type=text]");
+    const inputFieldValue = inputField ? inputField.value : null;
 
-      const inputField = document.querySelector("input[name='location']");
-      const inputFieldValue = inputField ? inputField.value : null;
+    console.log("Content Script: Input Field Value", inputFieldValue);
 
-      console.log("Content Script: Input Field Value", inputFieldValue);
-
-      if (inputFieldValue !== null) {
-        // Input field is populated, send the message to the background script
-        chrome.runtime.sendMessage({ action: "sendAllText", allText, inputFieldValue });
-      } else {
-        // Input field is not populated yet, wait for a short delay and try again
-        setTimeout(waitForInputField, 500);
-      }
-    }
-
-    // Retrieve the main text from the body
-    const allText = document.body.innerText;
-
-    // Start waiting for the input field
-    waitForInputField();
+    // Send the message to the background script
+    chrome.runtime.sendMessage({ action: "sendAllText", allText, inputFieldValue });
   }
 });
