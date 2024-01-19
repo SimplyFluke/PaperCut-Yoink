b3b22a51750d4ba8500cfe55d7ec33b118f90247
Dadle Duppsko <76550302+SimplyFluke@users.noreply.github.com>
2024-01-19 07:57:59 +0100

    Update background.js

--- background.js
+++ background.js
@@ -9,21 +9,30 @@ chrome.action.onClicked.addListener((tab) => {
   });
 });
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === "contentMessage") {
-    const message = request.message;
-    console.log("Background Script: Received Message from Content Script:", message);
+chrome.runtime.onMessage.addListener((request, sender) => {
+  console.log("Background Script: Received Message", request);
+
+  if (request.action === "sendAllText" && sender.tab) {
+    receivedAllText = request.allText;
+    const inputFieldValue = request.inputFieldValue;
+    console.log('Received All Text in Background:', receivedAllText);
+    console.log('Received input: ', inputFieldValue);
+
+    // Check if the URL contains the expected substring
+    if (sender.tab.url.toLowerCase().includes("ansatt")) {
+      console.log("Background Script: Checking URL - URL includes 'ansatt'");
+      processClipboardText(receivedAllText, sender.tab.id);
+    } else {
+      console.log("Background Script: Checking URL - URL does not include 'ansatt'");
+    }
   }
 });
 
 function sendTextToContentScript() {
-  const message = "Hello from Background!";
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    const activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, { action: "backgroundMessage", message });
-  });
+  const allText = document.body.innerText;
+  console.log("Background Script: Sending Message to Content Script");
+  chrome.runtime.sendMessage({ action: "sendAllText", allText });
 }
-
 
 function processClipboardText(clip, tabId) {
   // Your existing code for processing the clipboard text
@@ -60,3 +69,12 @@ function copyToClipboard(text) {
 String.prototype.capitalize = function () {
   return this.charAt(0).toUpperCase() + this.slice(1);
 };
+
+
+// // Input box from hell: // // 
+
+// Full XPATH: /html/body/div[5]/div[2]/div/div/div[2]/div[2]/table/tbody/tr[2]/td[2]/form/table/tbody/tr[1]/td/p[4]/input
+// JS Path: document.querySelector("#content > div > div > div.tabContent > div.box > table > tbody > tr:nth-child(2) > td.box-content > form > table > tbody > tr:nth-child(1) > td > p:nth-child(5) > input[type=text]")
+// Outer HTML: <input type="text" name="location" value="Sletten barnehage, Hallsetvegen 8, sokkel - innenfor arbeidsrom" size="40" maxlength="255">
+// Selector: #content > div > div > div.tabContent > div.box > table > tbody > tr:nth-child(2) > td.box-content > form > table > tbody > tr:nth-child(1) > td > p:nth-child(5) > input[type=text]
+// Element: <input type="text" name="location" value="Sletten barnehage, Hallsetvegen 8, sokkel - innenfor arbeidsrom" size="40" maxlength="255">
