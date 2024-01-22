34526f94c5c663fa4d48dbe5cf26e8c221423c44
Dadle Duppsko <76550302+SimplyFluke@users.noreply.github.com>
2024-01-22 10:22:55 +0100

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,16 +1,64 @@
-console.log("Content Script Executed");
+console.log("Content Script: Received readAllText message"); 
+ 
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) { 
+  if (request.action === "readAllText") { 
+    console.log("Content Script: Received readAllText message"); 
+ 
+    // Function to retrieve the input field value using XPATH 
+    function getInputFieldValueByXpath(xpath, delay = 2000) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          let element = null;
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === "readAllText") {
-    console.log("Content Script: Received readAllText message");
+          // Search in iframes first
+          const iframes = document.getElementsByTagName('iframe');
+          for (let i = 0; i < iframes.length; i++) {
+            const iframeDoc = iframes[i].contentDocument || iframes[i].contentWindow.document;
+            element = iframeDoc.evaluate(
+              xpath,
+              iframeDoc,
+              null,
+              XPathResult.FIRST_ORDERED_NODE_TYPE,
+              null
+            ).singleNodeValue;
+            if (element) {
+              break;
+            }
+          }
 
-    // Use the provided JS path to locate the input field
-    const inputField = document.querySelector("#content > div > div > div.tabContent > div.box > table > tbody > tr:nth-child(2) > td.box-content > form > table > tbody > tr:nth-child(1) > td > p:nth-child(5) > input[type=text]");
-    const inputFieldValue = inputField ? inputField.value : null;
+          // If the element wasn't found in the iframes, search in the main document
+          if (!element) {
+            element = document.evaluate(
+              xpath,
+              document,
+              null,
+              XPathResult.FIRST_ORDERED_NODE_TYPE,
+              null
+            ).singleNodeValue;
+          }
 
-    console.log("Content Script: Input Field Value", inputFieldValue);
+          console.log(element);
+          resolve(element ? element.value : null);
+        }, delay);
+      });
+    }
+ 
+    // XPATH for the input field 
+    const xpath = '//input[@name="location"]'; 
+ 
+    // Retrieve the main text from the body 
+    const allText = document.body.innerText; 
+ 
+    // Retrieve the input field value using XPATH 
+    const inputFieldValue = getInputFieldValueByXpath(xpath); 
+ 
+    // Retrieve the input field value using XPATH
+    getInputFieldValueByXpath(xpath).then((inputFieldValue) => {
+      console.log("Content Script: Input Field Value", inputFieldValue);
 
-    // Send the message to the background script
-    chrome.runtime.sendMessage({ action: "sendAllText", allText, inputFieldValue });
-  }
+      // Send the message directly to the background script
+      chrome.runtime.sendMessage({ action: "sendAllText", allText, inputFieldValue });
+    });
+
+  } 
 });
